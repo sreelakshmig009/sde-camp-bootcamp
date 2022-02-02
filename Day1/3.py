6cd763567636f8e8aa0de69b10e00109b4932f15
Sreelakshmi G <sreelakshmig009@gmail.com>
2022-02-02 16:55:03 +0530

    Create 3.py

---
+++ Day1/3.py
@@ -0,0 +1,9 @@
+# using for loop
+for i in range(1,100+1): # mentioned 100+1 to include the value 100 in the print statement
+  print(i) # print(i,end = " ") to print the values in the same line
+
+# using while loop
+i = 1
+while i>100:
+  print(i)
+  i += 1 # incrementing i value
