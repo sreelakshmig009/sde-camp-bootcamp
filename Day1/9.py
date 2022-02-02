7d45c180150a6b2ee78259c135441111430de1b9
Sreelakshmi G <sreelakshmig009@gmail.com>
2022-02-02 17:26:21 +0530

    Create 9.py

---
+++ Day1/9.py
@@ -0,0 +1,22 @@
+# type casting in python
+
+x = bool(1)   # x will be 1
+print(type(x))
+y = int(2.8) # y will be 2
+print(type(y))
+z = int("3") # z will be 3
+print(type(z))
+a = float(1)  # x will be 1.0
+print(type(a))
+b = float(2.8) # y will be 2.8
+print(type(b))
+c = float("3") # z will be 3.0
+print(type(c))
+d= float("4.2") # w will be 4.2
+print(type(d))
+e = str("s1") # x will be 's1'
+print(type(e))
+f = str(2) # y will be '2'
+print(type(f))
+g = str(3.0)  # z will be '3.0'
+print(type(g))
