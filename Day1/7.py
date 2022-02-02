e98cbbfb8ac89e62941e33924d0350d6f161a5ec
Sreelakshmi G <sreelakshmig009@gmail.com>
2022-02-02 17:19:00 +0530

    Create 7.py

---
+++ Day1/7.py
@@ -0,0 +1,14 @@
+# sum of n natural numbers
+
+num = int(input())
+sums = 0
+for n in range(1,num+1):
+  sums += n
+print(sums)
+#this method will create out timeout error in python online compilers if a value like 10324878728912817834718498478012 is given
+
+# mathematical formula
+print(num*(num+1)//2)
+  
+  
+  
